f83b9653c8e22764aa5419ef2404b0cab82645e1
neka-nat <nekanat.stock@gmail.com>
2024-11-06 10:15:38 +0900

    download model

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,8 @@ COPY pyproject.toml uv.lock ./
 RUN uv sync --frozen --no-cache
 
 COPY app.py ./
-COPY download_models.py ./
+COPY download_models.py test.pdf ./
 
-RUN /app/.venv/bin/python download_models.py
+RUN /app/.venv/bin/python download_models.py && rm download_models.py test.pdf
 
 CMD ["/app/.venv/bin/fastapi", "run", "app.py", "--port", "8000", "--host", "0.0.0.0"]
--- download_models.py
+++ download_models.py
@@ -2,10 +2,4 @@ import tempfile
 from docling.document_converter import DocumentConverter
 
 converter = DocumentConverter()
-try:
-    with tempfile.NamedTemporaryFile(delete=True) as temp_file:
-        temp_file.write(b"Hello, World!")
-        temp_file_path = temp_file.name
-        converter.convert(temp_file_path)
-except Exception as e:
-    print(e)
+converter.convert("test.pdf")
